c94e37960b32a0b3ce56e81dc59515fd76239c4e
Daniel Corson <dcorson@facebook.com>
2009-03-06 23:21:18 -0800

    pretty-printer: & in var_dump() can appear for any type; not just array
    Summary: irnoob; just realized this
    
    Reviewed By: eletuchy
    
    Test Plan: the new test and =$GLOBALS
    
    Revert Plan: ok

--- src/phpsh.php
+++ src/phpsh.php
@@ -89,16 +89,15 @@ if (!function_exists('___phpsh___pretty_print')) {
       $_depth=0) {
     static $indent_str = '  ';
     $depth_str = str_repeat($indent_str, $_depth);
-    $arr_prefix = '';
     // ad hoc parsing not very fun.. use lemon or something?  or is that overkill
     switch ($dump[$pos]) {
     case 'N':
       ___phpsh___parse_dump_assert($dump, $pos, 'NULL');
       return 'null';
     case '&':
-      $arr_prefix = '&';
       $pos++;
-      // intentional fall-thru
+      return '&'.___phpsh___parse_dump($x, $dump, $pos, $normal_end_check,
+        $_depth);
     case 'a':
       ___phpsh___parse_dump_assert($dump, $pos, 'array');
       $arr_len = (int)___phpsh___parse_dump_delim_grab($dump, $pos, false);
@@ -108,7 +107,7 @@ if (!function_exists('___phpsh___pretty_print')) {
       ___phpsh___parse_dump_assert($dump, $pos, $depth_str."}",
         $normal_end_check);
       return implode("\n", array_merge(
-        array($arr_prefix.'array('),
+        array('array('),
         $arr_lines,
         array($depth_str.')')
       ));
@@ -266,52 +265,57 @@ if (!function_exists('___phpsh___pretty_print')) {
     assert(___phpsh___assert_eq($i, '___phpsh___pretty_print',
       null,
       'null'
-      ));
+    ));
     assert(___phpsh___assert_eq($i, '___phpsh___pretty_print',
       true,
       'true'
-      ));
+    ));
     assert(___phpsh___assert_eq($i, '___phpsh___pretty_print',
       false,
       'false'
-      ));
+    ));
     assert(___phpsh___assert_eq($i, '___phpsh___pretty_print',
       4,
       '4'
-      ));
+    ));
     assert(___phpsh___assert_eq($i, '___phpsh___pretty_print',
       3.14,
       '3.14'
-      ));
+    ));
     assert(___phpsh___assert_eq($i, '___phpsh___pretty_print',
       "A\\\"'\'B\nC",
       '"A\\\\\"\'\\\\\'B\\nC"'
-      ));
+    ));
     assert(___phpsh___assert_re($i, '___phpsh___pretty_print',
       fopen('phpshtest.deleteme', 'w'),
       '<resource #\d+ of type stream>'
-      ));
+    ));
     assert(___phpsh___assert_eq($i, '___phpsh___pretty_print',
       array(04 => 'lol', '04' => 'lolo'),
       "array(\n  4 => \"lol\",\n  \"04\" => \"lolo\",\n)"
-      ));
+    ));
     $arr = array();
     $arr['self'] = $arr;
     // note the manifested depth might actually be variable and unknowable.
     // so we may have to loosen this test..
     assert(___phpsh___assert_eq($i, '___phpsh___pretty_print', $arr,
       "array(\n  \"self\" => array(\n    \"self\" => *RECURSION*,\n  ),\n)"
-      ));
+    ));
     $arr = array();
     $arr['fake'] = "Array\n *RECURSION*";
     $arr['sref'] = &$arr;
     $arr['self'] = $arr;
     assert(___phpsh___assert_eq($i, '___phpsh___pretty_print', $arr,
       "array(\n  \"fake\" => \"Array\\n *RECURSION*\",\n  \"sref\" => &array(\n    \"fake\" => \"Array\\n *RECURSION*\",\n    \"sref\" => &array(\n      \"fake\" => \"Array\\n *RECURSION*\",\n      \"sref\" => *RECURSION*,\n      \"self\" => array(\n        \"fake\" => \"Array\\n *RECURSION*\",\n        \"sref\" => *RECURSION*,\n        \"self\" => array(\n          \"fake\" => \"Array\\n *RECURSION*\",\n          \"sref\" => *RECURSION*,\n          \"self\" => *RECURSION*,\n        ),\n      ),\n    ),\n    \"self\" => array(\n      \"fake\" => \"Array\\n *RECURSION*\",\n      \"sref\" => &array(\n        \"fake\" => \"Array\\n *RECURSION*\",\n        \"sref\" => *RECURSION*,\n        \"self\" => array(\n          \"fake\" => \"Array\\n *RECURSION*\",\n          \"sref\" => *RECURSION*,\n          \"self\" => *RECURSION*,\n        ),\n      ),\n      \"self\" => array(\n        \"fake\" => \"Array\\n *RECURSION*\",\n        \"sref\" => &array(\n          \"fake\" => \"Array\\n *RECURSION*\",\n          \"sref\" => *RECURSION*,\n          \"self\" => *RECURSION*,\n        ),\n        \"self\" => *RECURSION*,\n      ),\n    ),\n  ),\n  \"self\" => array(\n    \"fake\" => \"Array\\n *RECURSION*\",\n    \"sref\" => &array(\n      \"fake\" => \"Array\\n *RECURSION*\",\n      \"sref\" => &array(\n        \"fake\" => \"Array\\n *RECURSION*\",\n        \"sref\" => *RECURSION*,\n        \"self\" => array(\n          \"fake\" => \"Array\\n *RECURSION*\",\n          \"sref\" => *RECURSION*,\n          \"self\" => *RECURSION*,\n        ),\n      ),\n      \"self\" => array(\n        \"fake\" => \"Array\\n *RECURSION*\",\n        \"sref\" => &array(\n          \"fake\" => \"Array\\n *RECURSION*\",\n          \"sref\" => *RECURSION*,\n          \"self\" => *RECURSION*,\n        ),\n        \"self\" => *RECURSION*,\n      ),\n    ),\n    \"self\" => array(\n      \"fake\" => \"Array\\n *RECURSION*\",\n      \"sref\" => &array(\n        \"fake\" => \"Array\\n *RECURSION*\",\n        \"sref\" => &array(\n          \"fake\" => \"Array\\n *RECURSION*\",\n          \"sref\" => *RECURSION*,\n          \"self\" => *RECURSION*,\n        ),\n        \"self\" => *RECURSION*,\n      ),\n      \"self\" => *RECURSION*,\n    ),\n  ),\n)"
-      ));
+    ));
     assert(___phpsh___assert_eq($i, '___phpsh___pretty_print',
       array('a[b]c' => 4),
       "array(\n  \"a[b]c\" => 4,\n)"
+    ));
+    $var_to_ref = 4;
+    assert(___phpsh___assert_eq($i, '___phpsh___pretty_print',
+      array('hi' => &$var_to_ref),
+      "array(\n  \"hi\" => &4,\n)"
     ));
     return $i;
   }
