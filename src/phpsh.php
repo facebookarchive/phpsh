facebookarchive/phpsh

commit 4afa0afe4396581f4950bda9a622cd41512ada60

Show exception stack traces in phpsh

src/phpsh.php
@@ -493,6 +493,24 @@ class ___Phpsh___ {
   }
 
   /**
+   * eval with exception handling
+   */
+  function eval_and_handle($buffer) {
+    try {
+      $evalue = eval($buffer);
+    } catch (Exception $e) {
+      // unfortunately, almost all exceptions that aren't explicitly
+      // thrown by users are uncatchable :(
+      fwrite(STDERR,
+             'Uncaught exception: '.get_class($e).': '.
+             $e->getMessage()."\n".
+             $e->getTraceAsString()."\n");
+      $evalue = null;
+    }
+    return $evalue;
+  }
+
+  /**
    * The main interactive loop
    *
    * @author    ccheever
@@ -547,30 +565,13 @@ class ___Phpsh___ {
         if ($pid) {
           pcntl_wait($status);
         } else {
-          try {
-            $evalue = eval($buffer);
-          } catch (Exception $e) {
-            // unfortunately, almost all exceptions that aren't explicitly
-            // thrown by users are uncatchable :(
-            fwrite(STDERR, 'Uncaught exception: '.get_class($e).': '.
-              $e->getMessage()."\n");
-            $evalue = null;
-          }
-
+          $evalue = $this->eval_and_handle($buffer);
           // if we are still alive..
           $childpid = posix_getpid();
           fwrite($this->_comm_handle, "child $childpid\n");
         }
       } else {
-        try {
-          $evalue = eval($buffer);
-        } catch (Exception $e) {
-          // unfortunately, almost all exceptions that aren't explicitly thrown
-          // by users are uncatchable :(
-          fwrite(STDERR, 'Uncaught exception: '.get_class($e).': '.
-            $e->getMessage()."\n");
-          $evalue = null;
-        }
+        $evalue = $this->eval_and_handle($buffer);
       }
 
       if ($buffer != "xdebug_break();\n") {

commit de65124869f9c845e8c51a8d7efff6078d85fcbf

Fixed a bug with assignment introduced in 4afa0afe4396581f4950.
Refactoring the `eval()` statements to a separate function won't work as assignment in phpsh relies on `eval()` scoping variables inside `___Phpsh___::interactive_loop()`.

src/phpsh.php
@@ -493,24 +493,6 @@ class ___Phpsh___ {
   }
 
   /**
-   * eval with exception handling
-   */
-  function eval_and_handle($buffer) {
-    try {
-      $evalue = eval($buffer);
-    } catch (Exception $e) {
-      // unfortunately, almost all exceptions that aren't explicitly
-      // thrown by users are uncatchable :(
-      fwrite(STDERR,
-             'Uncaught exception: '.get_class($e).': '.
-             $e->getMessage()."\n".
-             $e->getTraceAsString()."\n");
-      $evalue = null;
-    }
-    return $evalue;
-  }
-
-  /**
    * The main interactive loop
    *
    * @author    ccheever
@@ -565,13 +547,30 @@ class ___Phpsh___ {
         if ($pid) {
           pcntl_wait($status);
         } else {
-          $evalue = $this->eval_and_handle($buffer);
+          try {
+            $evalue = eval($buffer);
+          } catch (Exception $e) {
+            // unfortunately, almost all exceptions that aren't explicitly
+            // thrown by users are uncatchable :(
+            fwrite(STDERR, 'Uncaught exception: '.get_class($e).': '.
+              $e->getMessage()."\n".$e->getTraceAsString()."\n");
+            $evalue = null;
+          }
+
           // if we are still alive..
           $childpid = posix_getpid();
           fwrite($this->_comm_handle, "child $childpid\n");
         }
       } else {
-        $evalue = $this->eval_and_handle($buffer);
+        try {
+          $evalue = eval($buffer);
+        } catch (Exception $e) {
+          // unfortunately, almost all exceptions that aren't explicitly thrown
+          // by users are uncatchable :(
+          fwrite(STDERR, 'Uncaught exception: '.get_class($e).': '.
+            $e->getMessage()."\n".$e->getTraceAsString()."\n");
+          $evalue = null;
+        }
       }
 
       if ($buffer != "xdebug_break();\n") {