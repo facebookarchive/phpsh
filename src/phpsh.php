6c47441928fa638caae4148f94da1f15ec80b494
Jacek Spera <jacek.spera@webresint.com>
2011-05-31 10:32:37 +0000

    got rid of dependency on posix module; replaced posix_getpid with getmypid

--- src/phpsh.php
+++ src/phpsh.php
@@ -25,7 +25,7 @@ if (version_compare(PHP_VERSION, '5.0.0', '<')) {
   exit;
 }
 
-$missing = array_diff(array('pcre','posix','tokenizer'),
+$missing = array_diff(array('pcre','tokenizer'),
                       get_loaded_extensions());
 if ($missing) {
   fwrite(STDERR, 'Fatal error: phpsh requires the following extensions: '.
@@ -568,7 +568,7 @@ class ___Phpsh___ {
       }
 
       if ($this->fork_every_command) {
-        $parent_pid = posix_getpid();
+        $parent_pid = getmypid();
         $pid = pcntl_fork();
         $evalue = null;
         if ($pid) {
@@ -585,7 +585,7 @@ class ___Phpsh___ {
           }
 
           // if we are still alive..
-          $childpid = posix_getpid();
+          $childpid = getmypid();
           fwrite($this->_comm_handle, "child $childpid\n");
         }
       } else {
